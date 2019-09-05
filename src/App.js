a1d989ec5f69719dbed7daba3c285abc438b502e
Angel Hernandez Licona <angel.hernandez@televisatim.com>
2019-09-05 12:54:02 -0500

    FEAT: Se agrego el paquete react-gtp para probarlo
    es una implementacion de GPT

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {Bling as GPT} from "react-gpt";
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <GPT
+          adUnitPath="/4595/nfl.test.open"
+          slotSize={[728, 90]}
+        />
+
         <a
           className="App-link"
           href="https://reactjs.org"
